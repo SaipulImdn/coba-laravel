59f89ca83c05aa9bea9f8f8ed0c790e1ade9c8ff
SaipulImdn <yusriltc@gmail.com>
2023-06-24 12:59:06 +0700

    PERUBAHAN 1

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Syaiful Imanudin | About</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('container')
     <h1>Halaman About</h1>
-    <h3><?php echo $name; ?></h3>
-    <p><?= $email; ?></p>
-    <img src="img/<?= $image; ?>" alt="<?= $name; ?>" width="200px">
-    
-</body>
-</html>
+    <h3>{{ $name }}</h3>
+    <p>{{  $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200px">
+    @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/About', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Syaiful Imanudin",
         "email" => "yusriltc@gmail.com",
         "image" => "CV.jpg"
@@ -26,7 +29,9 @@ Route::get('/About', function () {
 });
 
 Route::get('/Profile', function () {
-    return view('profile');
+    return view('profile', [
+        "title" => "profile"
+    ]);
 });
 
 
