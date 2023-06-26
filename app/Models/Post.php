e147d22ade2fcbfb11f045af4df27327295121ba
SaipulImdn <yusriltc@gmail.com>
2023-06-26 22:15:55 +0700

    penambahan dan perubahan

---
+++ app/Models/Post.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+
+class Post 
+{
+    private static $blog_posts  = [
+        [
+            "title" => "Judu Tulisan Pertama",
+            "slug" => "judul-tulisan-pertama",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint placeat totam officia accusamus expedita odio iste. Sed voluptas aliquam illo enim iusto, dignissimos maiores recusandae dolor praesentium officiis quae harum."
+        ],
+        [
+            "title" => "Judul Tulisan Kedua",
+            "slug" => "judul-tulisan-kedua",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint placeat totam officia accusamus expedita odio iste. Sed voluptas aliquam illo enim iusto, dignissimos maiores recusandae dolor praesentium officiis quae harum."
+        ]
+        ];
+
+        public static function all()
+        {
+            return self::$blog_posts;
+        }
+
+        public static function find($slug)
+        {
+            $posts = self::$blog_posts;
+         $post = [];
+     foreach($posts as $p) {
+         if($p["slug"] === $slug) {
+             $post = $p;
+         }
+    }
+    return $post;
+        }
+}
